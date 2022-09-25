7933c8bc824c3c8d20ba762541d8f0825b91ff46
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-09-25 18:39:46 +0200

    added tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode:"jit",
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-      ],
+    content: ['./modules/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
