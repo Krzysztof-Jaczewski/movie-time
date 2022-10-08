f0ac34ca3b582d5833f21b05dde75e873616b5e3
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-10-08 11:57:56 +0200

    added Logo to Header

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -3,14 +3,13 @@ import Image from 'next/image';
 import HeaderIcon from 'components/atoms/HeaderIcon';
 import { headerIconProps } from 'configs/header';
 import Link from 'components/atoms/Link';
-import Logo from 'components/atoms/Logo/Logo';
+import Logo from 'components/atoms/Logo';
 
 // import HeaderIcon from './HeaderIcon';
 
 const Header: FC = () => {
   return (
     <header className='flex flex-col gap-5 sm:flex-row m-5 justify-between items-center'>
-      {/* <h1>Movies</h1> */}
       <div className='flex flex-grow justify-between max-w-xl'>
         {headerIconProps.map(
           ({ title, Icon }) => (
